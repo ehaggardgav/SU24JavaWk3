845e730b67c3a523ad42cca39ae4ba0dc36d14bb
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-06-26 22:20:08 +0000

    Constructed student roster input prompt with a counter variable to number the entries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,22 @@ import java.util.ArrayList;
 public class Main {
 public static void main(String[] args) {
 //Create dynamic array to hold student names
-ArrayList<String> students = new ArrayList<>();
+
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Student Roster");
+System.out.println(""); 
   
-  
-    
-  }
+String name = "";
+int studentNum = 1;
+  do {
+    System.out.print("Student" + studentNum + ": ");
+    name = scan.nextLine();
+    studentNum++;
+  } while (!name.equals("x"));
+
+
+}
     //System.out.println(nums_arr[3]);
 
   // @Test
